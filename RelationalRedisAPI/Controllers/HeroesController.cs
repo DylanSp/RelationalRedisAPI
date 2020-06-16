525f27b0e8f09c92ff7b9fd7e0dfe49a27bdadc7
Dylan Sprague <dylan.richard.sprague@gmail.com>
2020-06-16 01:00:09 -0400

    Reorder code in HeroesController

--- RelationalRedisAPI/Controllers/HeroesController.cs
+++ RelationalRedisAPI/Controllers/HeroesController.cs
@@ -53,23 +53,9 @@ namespace RelationalRedisAPI.Controllers
             return CreatedAtRoute(nameof(Get), new {hero.Id}, hero);
         }
 
-        // DELETE api/heroes/32c64485-d35c-4a01-b412-06a9cb84c19c
-        [HttpDelete("{id:guid}")]
-        public ActionResult Delete([FromRoute] Guid id)
-        {
-            var heroExists = HeroAdapter.Read(id).HasValue;
-            if (!heroExists)
-            {
-                return NotFound();
-            }
-
-            HeroAdapter.Delete(id);
-            return NoContent();
-        }
-
         // POST api/heroes/32c64485-d35c-4a01-b412-06a9cb84c19c
         [HttpPost("{id:guid}")]
-        public ActionResult<Hero> Update([FromRoute]Guid id, [FromBody] Hero hero)
+        public ActionResult<Hero> Update([FromRoute] Guid id, [FromBody] Hero hero)
         {
             var heroExists = HeroAdapter.Read(id).HasValue;
             if (!heroExists)
@@ -84,6 +70,20 @@ namespace RelationalRedisAPI.Controllers
 
             HeroAdapter.Save(hero);
             return hero;
+        }
+
+        // DELETE api/heroes/32c64485-d35c-4a01-b412-06a9cb84c19c
+        [HttpDelete("{id:guid}")]
+        public ActionResult Delete([FromRoute] Guid id)
+        {
+            var heroExists = HeroAdapter.Read(id).HasValue;
+            if (!heroExists)
+            {
+                return NotFound();
+            }
+
+            HeroAdapter.Delete(id);
+            return NoContent();
         }
     }
 }
