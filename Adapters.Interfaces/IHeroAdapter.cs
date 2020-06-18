cc8b79a78d30793d0790e083bb4635fbd334589a
Dylan Sprague <dylan.richard.sprague@gmail.com>
2020-06-18 03:57:36 -0400

    Add note on testing SearchHeroes()

--- Adapters.Interfaces/IHeroAdapter.cs
+++ Adapters.Interfaces/IHeroAdapter.cs
@@ -6,6 +6,7 @@ namespace Adapters.Interfaces
 {
     public interface IHeroAdapter : IEntityAdapter<Hero>
     {
+        // TODO - tests for this in SQL/Redis adapters
         IEnumerable<Hero> SearchHeroes(string name, string location);   // TODO - search by power name and/or ID?
     }
 }
